a30e762c14ba0c99e62c1c07bd060b9f9ff658e3
Vladislav Naraikin <randalv@yandex.ru>
2025-06-11 01:39:41 +0700

    disable routing

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,13 +1,13 @@
-import About from "@/pages/About";
+//import About from "@/pages/About";
 import Home from "@/pages/Home";
-import NoMatch from "@/pages/NoMatch";
+//import NoMatch from "@/pages/NoMatch";
 import type { ReactNode } from "react";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 const appRouter = createBrowserRouter(createRoutesFromElements([
-      <Route path="/" element={<Home />} />,
-      <Route path="about/*" element={<About />} />,
-      <Route path="/*" element={<NoMatch />} />
+      <Route path="/*" element={<Home />} />,
+      //<Route path="about/*" element={<About />} />,
+      //<Route path="/*" element={<NoMatch />} />
 ]))
 
 const AppProvider = (): ReactNode => {
--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,13 +1,17 @@
-import { Link } from "react-router-dom"
+//import { Link } from "react-router-dom"
+
+/*
+<nav className="header__nav">
+            <Link to="/" className="header-nav__link">Главная</Link>
+            <Link to="/about" className="header-nav__link">О задании</Link>
+        </nav>
+*/
 
 function header() {
   return (
     <header className="header">
         <span className="header__title">ТЗ Artix | Нарайкин В.В.</span>
-        <nav className="header__nav">
-            <Link to="/" className="header-nav__link">Главная</Link>
-            <Link to="/about" className="header-nav__link">О задании</Link>
-        </nav>
+        
     </header>
   )
 }
