b7f4fa77be1648e214c71c5813ea65a8e0a1da3f
Vladislav Naraikin <randalv@yandex.ru>
2025-06-12 15:30:08 +0700

    hash router

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,13 +1,13 @@
-//import About from "@/pages/About";
+import About from "@/pages/About";
 import Home from "@/pages/Home";
-//import NoMatch from "@/pages/NoMatch";
+import NoMatch from "@/pages/NoMatch";
 import type { ReactNode } from "react";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createHashRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
-const appRouter = createBrowserRouter(createRoutesFromElements([
+const appRouter = createHashRouter(createRoutesFromElements([
       <Route path="/*" element={<Home />} />,
-      //<Route path="about/*" element={<About />} />,
-      //<Route path="/*" element={<NoMatch />} />
+      <Route path="about/*" element={<About />} />,
+      <Route path="/*" element={<NoMatch />} />
 ]))
 
 const AppProvider = (): ReactNode => {
